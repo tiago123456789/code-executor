8c9b8d648d1f0a7d0e876f5bc6fec24b5486ef5d
Tiago rosa da costa <tiagorosadacost@gmail.com>
2024-01-27 17:18:15 -0300

    configured logger using winston and putted logs on cronjob responsable to define run or not scripts

--- api/src/jobs/SendScriptToRun.js
+++ api/src/jobs/SendScriptToRun.js
@@ -1,30 +1,41 @@
+const logger = require("../config/Logger")
 const { QUEUE_SCRIPT_RUN } = require("../constants/Queue")
 const queue = require("../config/Queue")(QUEUE_SCRIPT_RUN)
 const ScriptRepository = require("../repositories/ScriptRepository")
 
+
 const scriptRepository = new ScriptRepository()
 
 module.exports = async () => {
-    console.log("Start process to get scripts needs to run")
-    const scripts = await scriptRepository.getScriptsToExecute();
+    try {
+        logger.info("Start process to get scripts needs to run")
+        logger.info("Gettings scripts needs to execute now")
+        const scripts = await scriptRepository.getScriptsToExecute();
+        logger.info(`Total ${scripts.length} scripts needs to execute now`)
 
-    if (scripts.length == 0) {
-        console.log("Finished here because don't have nothing to process")
-        return;
+        if (scripts.length == 0) {
+            logger.info("Finished here because don't have nothing to process")
+            return;
+        }
+
+        logger.info(`Sending data about scripts needs to execute now to queue`)
+        await queue.addBulk(scripts, {
+            attempts: 2,
+            removeOnComplete: true
+        })
+
+        const scriptIds = []
+        for (let index = 0; index < scripts.length; index += 1) {
+            scriptIds.push(scripts[index].id)
+        }
+
+        logger.info(`Updating column last_execution all scripts sended to queue`)
+        await scriptRepository.updateMany(scriptIds, {
+            last_execution: new Date()
+        })
+        logger.info("Finished process to get scripts needs to run")
+    } catch (error) {
+        logger.error(error.message)
     }
 
-    await queue.addBulk(scripts, {
-        attempts: 2,
-        removeOnComplete: true
-    })
-
-    const scriptIds = []
-    for (let index = 0; index < scripts.length; index += 1) {
-        scriptIds.push(scripts[index].id)
-    }
-
-    await scriptRepository.updateMany(scriptIds, {
-        last_execution: new Date()
-    })
-    console.log("Finished process to get scripts needs to run")
 }
--- api/src/jobs/index.js
+++ api/src/jobs/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config({ path: ".env" })
+const logger = require("../config/Logger")
 const schedule = require('node-schedule');
 const sendScriptToRun = require("./SendScriptToRun");
 
 schedule.scheduleJob('*/10 * * * * *', sendScriptToRun);
 
+logger.info("Start cronjobs")
