77673a25bc1dd166672e7b54edccc35bf1d270f7
Tiago rosa da costa <tiagorosadacost@gmail.com>
2024-02-10 12:35:25 -0300

    changes validation to accept function start usins async keyword

--- api/src/utils/Code.js
+++ api/src/utils/Code.js
@@ -1,6 +1,6 @@
 
 const hasFunctionStart = (code) => {
-    return /^(function start\s\(|function start\()/gm.test(code)
+    return /^(async\sfunction start\s\(|async\sfunction start\(|function start\s\(|function start\()/gm.test(code)
 }
 
 module.exports = { hasFunctionStart }
